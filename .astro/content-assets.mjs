243f4b629b7105d0a90a99475fe267d4696ca8e6
felix <felix.soewito@ninjavan.co>
2025-07-23 15:02:35 +0700

    first commit

---
+++ .astro/content-assets.mjs
@@ -0,0 +1,4 @@
+
+import __ASTRO_IMAGE_IMPORT_Z1isoKR from "../../assets/blog-placeholder-1.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fusing-mdx.mdx";
+export default new Map([["../../assets/blog-placeholder-1.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fusing-mdx.mdx", __ASTRO_IMAGE_IMPORT_Z1isoKR]]);
+		
