9f2821b06ef4fd586e6fb8d40e6ff5a0923e18c7
felix <felix.soewito@ninjavan.co>
2025-07-24 15:13:59 +0700

    done with main page

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Lixionary';
-export const SITE_DESCRIPTION = 'Nice to meet you';
+export const SITE_DESCRIPTION = 'follow your life compass';
