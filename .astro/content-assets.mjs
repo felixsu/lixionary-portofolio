2ce6d30b167390dab2c6b69f149f82c9b4570d77
felix <felix.soewito@ninjavan.co>
2025-07-30 22:51:07 +0700

    improvement on the UI, half done on every content

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,4 +1,1 @@
-
-import __ASTRO_IMAGE_IMPORT_Z1isoKR from "../../assets/blog-placeholder-1.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fusing-mdx.mdx";
-export default new Map([["../../assets/blog-placeholder-1.jpg?astroContentImageFlag=&importer=src%2Fcontent%2Fblog%2Fusing-mdx.mdx", __ASTRO_IMAGE_IMPORT_Z1isoKR]]);
-		
+export default new Map();
